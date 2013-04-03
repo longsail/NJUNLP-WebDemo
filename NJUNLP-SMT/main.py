3af0a5cd2f9d9645587ff71bcabada7c2b7d19b0
longsail <yuanhang.nju@gmail.com>
2013-04-03 09:04:43 +0800

    NJUNLP main function

--- NJUNLP-SMT/main.py
+++ NJUNLP-SMT/main.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 import os.path 
 
 import tornado.web 
@@ -5,21 +6,37 @@ import tornado.httpserver
 import tornado.ioloop 
 import tornado.options
 
+from  suds.client import Client
+
 from tornado.options import define,options
 
 define("port",default=8000,help="run on the given port",type=int)
 
 class IndexHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render('index.html')
+	def ChiToEngTranslate(self,text):
+		WordSegUrl = 'http://114.212.189.224:9444/WCFICTCLASService?wsdl'
+		WordSegClient = Client(WordSegUrl)
 
+		SMTUrl = 'http://114.212.189.224:9000/NJUPhraZTranslationService/service?wsdl'
+		SMTClient = Client(SMTUrl)
+
+		ChiString = WordSegClient.service.RunICTCLASSegment(text)
+		EngTranString = SMTClient.service.Translate(ChiString)
+		return EngTranString
+
+	def get(self):
+		test_string = self.get_argument('test',"nanjing")
+		test_transtring = self.ChiToEngTranslate(test_string)
+		self.render('index.html',test_transtring=test_transtring)
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
 	app=tornado.web.Application(
-			handlers=[(r"/",IndexHandler)],
+			handlers=[(r'/',IndexHandler)
+			],
 			template_path=os.path.join(os.path.dirname(__file__),"templates"),
-			static_path=os.path.join(os.path.dirname(__file__),"static")
+			static_path=os.path.join(os.path.dirname(__file__),"static"),
+			debug=True
 			)
 	http_server=tornado.httpserver.HTTPServer(app)
 	http_server.listen(options.port)
