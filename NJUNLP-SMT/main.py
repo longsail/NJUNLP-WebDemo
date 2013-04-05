5ab96d59c70bf7f42f71b43e6e887ffa3780c45b
longsail <yuanhang.nju@gmail.com>
2013-04-05 20:48:59 +0800

    code refactoring

--- NJUNLP-SMT/main.py
+++ NJUNLP-SMT/main.py
@@ -13,33 +13,47 @@ from tornado.options import define,options
 
 define("port",default=8000,help="run on the given port",type=int)
 
-class IndexHandler(tornado.web.RequestHandler):
-	def ChiToEngTranslate(self,text):
-		WordSegUrl = 'http://114.212.189.224:9444/WCFICTCLASService?wsdl'
-		WordSegClient = Client(WordSegUrl)
+class baseHandler(tornado.web.RequestHandler):
+	def Regex(self,text):
+		regex = r'[\r]?\n'
+		reobj = re.compile(regex)
+		return reobj.sub('',text)
+				
+	def CHISegWebService(self,text):
+		CHISegUrl = 'http://114.212.189.224:9444/WCFICTCLASService?wsdl'
+		CHISegClient = Client(CHISegUrl)
 
+		text = self.Regex(text)
+		return CHISegClient.service.RunICTCLASSegment(text)
+
+	def SMTWebService(self,text):
 		SMTUrl = 'http://114.212.189.224:9000/NJUPhraZTranslationService/service?wsdl'
 		SMTClient = Client(SMTUrl)
 
-		ChiString = WordSegClient.service.RunICTCLASSegment(text)
-		EngTranString = SMTClient.service.Translate(ChiString)
-		return EngTranString
-	def SMTRegex(self,text):
-		regex = r'[\r]?\n'
-		reobj = re.compile(regex)
-		text = reobj.sub('',text)
-		return text
+		text = self.Regex(text)
+		text = self.CHISegWebService(text)
+		return SMTClient.service.Translate(text)
+
+class SmtHandler(baseHandler):
 
 	def get(self):
-		InputText = self.get_argument('InputText',"nanjing")
-		InputText = self.SMTRegex(InputText)
-		TransOutput = self.ChiToEngTranslate(InputText)
-		self.render('index.html',TransOutput = TransOutput)
+		inputText = self.get_argument('inputText','nanjing')
+		transOutput = self.SMTWebService(inputText)
+		self.render('index.html',output = transOutput)
+
+class ChiSegHandler(baseHandler):
+
+	def get(self):
+		inputText = self.get_argument('inputText','nanjing')
+		chinSegOutput = self.CHISegWebService(inputText)
+		self.render('index.html',output = chinSegOutput)
+
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
 	app=tornado.web.Application(
-			handlers=[(r'/',IndexHandler)
+			handlers=[(r'/',SmtHandler)
+				#(r'/',ChiSegHandler)
 			],
 			template_path=os.path.join(os.path.dirname(__file__),"templates"),
 			static_path=os.path.join(os.path.dirname(__file__),"static"),
