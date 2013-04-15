e12c04a6b4829665f7cd81e4af5ca87a6ea2f2af
longsail <yuanhang.nju@gmail.com>
2013-04-15 13:23:27 +0800

    main process for WebDemo

--- NJUNLP-SMT/main.py
+++ NJUNLP-SMT/main.py
@@ -1,9 +1,7 @@
 #coding: utf-8
 import os.path 
-import re
-from suds.client import Client
 import pymongo
-#from time import time
+from BaseHandler import BaseHandler
 
 import tornado.web 
 import tornado.httpserver
@@ -13,66 +11,7 @@ import tornado.options
 from tornado.options import define,options
 define("port",default=8000,help="run on the given port",type=int)
 
-class baseHandler(tornado.web.RequestHandler):
-
-	def Regex(self,text):
-		regex = r'[\r]?\n'
-		reobj = re.compile(regex)
-		return reobj.sub('',text)
-				
-	def CHISegWebService(self,text):
-		CHISegUrl = 'http://114.212.189.224:9444/WCFICTCLASService?wsdl'
-		CHISegClient = Client(CHISegUrl)
-
-		text = self.Regex(text)
-		return CHISegClient.service.RunICTCLASSegment(text)
-
-	def SMTWebService(self,text):
-		SMTUrl = 'http://114.212.189.224:9000/NJUPhraZTranslationService/service?wsdl'
-		SMTClient = Client(SMTUrl)
-
-		text = self.Regex(text)
-		text = self.CHISegWebService(text)
-		return SMTClient.service.Translate(text)
-
-	def saveResult(self,text,output,ip):
-		result = dict()
-
-		result["source"] = text
-		result["target"] = output
-		result["ip"] = ip
-
-		return result
-
-	def SelService(self,text,project,ip):
-		collsmt = self.application.db.testsmt
-		collseg = self.application.db.testseg
-
-		if project == "SMT":
-			result =  collsmt.find_one({"source":text})
-			if result:
-				return result["target"]
-			else:
-				output = self.SMTWebService(text)
-				SMTResult = self.saveResult(text,output,ip)
-				collsmt.insert(SMTResult)
-
-				return output
-			
-		elif project == "SEG": 
-			result = collseg.find_one({"source":text})
-			if result:
-				return result["target"]
-			else:
-				output = self.CHISegWebService(text)
-				SEGResult = self.saveResult(text,output,ip)
-				collseg.insert(SEGResult)
-
-				return output
-		else:
-			return "longsail Love Nju"
-
-class IndexHandler(baseHandler):
+class IndexHandler(BaseHandler):
 	def get(self):
 		inputText = self.get_argument('inputText','nanjing')
 		project = self.get_argument('project','SMT')
